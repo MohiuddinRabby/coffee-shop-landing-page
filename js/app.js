95fadd57208e2314066836f110d750ce6be6f01d
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-18 13:48:27 +0600

    banner design done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 // scrool reveal
 window.sr = ScrollReveal();
-sr.reveal('.navbar', {
+sr.reveal(".navbar", {
+  duration: 2000,
+});
+
+sr.reveal(".banner-slogan", {
+  duration: 2000,
+  origin: "top",
+  distance: "70px",
+});
+sr.reveal(".banner-buttons", {
+  duration: 3000,
+});
+
+sr.reveal(".banner-slogan-lorem", {
     duration: 2000,
+    origin: "bottom",
+    distance: "70px",
 });
